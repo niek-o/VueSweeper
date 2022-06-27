f2a83e1e1d0b1a460836c5c582dc9bdbe1961eb1
Niek <Frankenstein795@gmail.com>
2022-06-27 22:30:04 +0200

    Add Game class

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="flex flex-col">
     <div class="flex flex-row non-drag">
-      <h1 class="text-center text-4xl border-4 border-red text-red bg-darkRed w-48 h-12">{{ flags }}</h1>
+      <h1 class="text-center text-4xl border-4 border-red text-red bg-darkRed w-48 h-12">{{ game.flags }}</h1>
       <button
           class="w-48 text-4xl bg-cell border-4 border-t-borderLight border-l-borderLight border-b-borderDark border-r-borderDark h-12"
-          @click="initGame(width, height, bombs)">Restart
+          @click="game.initGame()">Restart
       </button>
     </div>
     <div>
       <div class="flex" v-for="rows in grid">
         <div v-for="cell in rows">
-          <Cell @click="dimCell(cell)"
-                @click.right="markFlag(cell)"
+          <Cell @click="game.clickCell(cell)"
+                @click.right="game.markFlag(cell)"
                 :cell="cell"></Cell>
         </div>
       </div>
     </div>
     <div class="w-96 h-12 text-center text-4xl bg-cell border-4 border-t-borderLight border-l-borderLight border-b-borderDark border-r-borderDark non-drag">
-      <h1 v-if="gameOver">Game Over!</h1>
-      <h1 v-if="win">You win!</h1>
+      <h1 v-if="game.gameOver">Game Over!</h1>
+      <h1 v-if="game.win">You win!</h1>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Cell from "../components/Cell.vue"
-import {initGame, dimCell, markFlag, grid, gameOver, win, flags} from "../utils/logic";
-import {onBeforeMount} from "vue";
+import {Game} from "../utils/logic";
 
 const props = defineProps<{
   width: number,
@@ -34,9 +33,9 @@ const props = defineProps<{
   bombs: number,
 }>()
 
-onBeforeMount(() => {
-  initGame(props.width, props.height, props.bombs)
-})
+const game = new Game(props.width, props.height, props.bombs)
+
+const grid = game.grid
 </script>
 
 <style scoped>
--- src/utils/logic.ts
+++ src/utils/logic.ts
@@ -1,290 +1,280 @@
-import {reactive, Ref, ref} from "vue";
+import {reactive} from "vue";
 import {CellType} from "./types";
 import {getRandomNumber} from "./getRandomNumber";
 
-let globalWidth = 0
-let globalHeight = 0
-let globalBombs = 0
-
-export const flags: Ref<number> = ref(globalBombs)
-
-export const gameOver: Ref<boolean> = ref(false)
-export const win: Ref<boolean> = ref(false)
-
-const moves: Ref<number> = ref(0)
-
-export const grid: CellType[][] = reactive([])
-
-const cellCount: Ref<number> = ref(globalWidth * globalHeight - globalBombs)
-
-function shortDim(cell: CellType) {
-    fakeDim(cell, true)
-    setTimeout(() => {
-        fakeDim(cell, false)
-    }, 75)
-}
-
-function fakeDim(cell: CellType, dimmed: boolean) {
-    const {r, l, br, b, bl, tr, t, tl} = getAdjacentTiles(cell)
-
-    if (r && !r.flag) r.fakeDimmed = dimmed
-    if (l && !l.flag) l.fakeDimmed = dimmed
-
-    if (grid[cell.row + 1]) { // check bottom row
-        if (br && !br.flag) br.fakeDimmed = dimmed
-        if (b && !b.flag) b.fakeDimmed = dimmed
-        if (bl && !bl.flag) bl.fakeDimmed = dimmed
-    }
-
-    if (grid[cell.row - 1]) { // check top row
-        if (tr && !tr.flag) tr.fakeDimmed = dimmed
-        if (t && !t.flag) t.fakeDimmed = dimmed
-        if (tl && !tl.flag) tl.fakeDimmed = dimmed
-    }
-}
-
-function checkAdjacentCells(cell: CellType) {
-    const flagsNearCell = getAmountOfFlagsAdjacentToCell(cell)
-    shortDim(cell)
-
-    if (flagsNearCell === cell.val) {
-        dimAdjacentTiles(cell)
-    }
-}
-
-function getAdjacentTiles(cell: CellType) {
-    const r = grid[cell.row][cell.col + 1]
-    const l = grid[cell.row][cell.col - 1]
-
-    let br, b, bl;
-
-    if (grid[cell.row + 1]) {
-        br = grid[cell.row + 1][cell.col + 1]
-        b = grid[cell.row + 1][cell.col]
-        bl = grid[cell.row + 1][cell.col - 1]
-    }
-
-    let tr, t, tl;
-
-    if (grid[cell.row - 1]) {
-        tr = grid[cell.row - 1][cell.col + 1]
-        t = grid[cell.row - 1][cell.col]
-        tl = grid[cell.row - 1][cell.col - 1]
-    }
-
-    return {r, l, br, b, bl, tr, t, tl}
-}
-
-function getAmountOfFlagsAdjacentToCell(cell: CellType) {
-    let flagsNearCell = 0;
-
-    const {r, l, br, b, bl, tr, t, tl} = getAdjacentTiles(cell)
-
-    if (r && r.flag) flagsNearCell++
-    if (l && l.flag) flagsNearCell++
-
-    if (grid[cell.row + 1]) { // check bottom row
-        if (br && br.flag) flagsNearCell++
-        if (b && b.flag) flagsNearCell++
-        if (bl && bl.flag) flagsNearCell++
-    }
-
-    if (grid[cell.row - 1]) { // check top row
-        if (tr && tr.flag) flagsNearCell++
-        if (t && t.flag) flagsNearCell++
-        if (tl && tl.flag) flagsNearCell++
-    }
-
-    return flagsNearCell
-}
-
-function dimAdjacentTiles(cell: CellType) {
-    const {r, l, br, b, bl, tr, t, tl} = getAdjacentTiles(cell)
-
-    if (r && !r.dimmed) dimCell(r)
-    if (l && !l.dimmed) dimCell(l)
-
-    if (grid[cell.row + 1]) { // check bottom row
-        if (br && !br.dimmed) dimCell(br)
-        if (b && !b.dimmed) dimCell(b)
-        if (bl && !bl.dimmed) dimCell(bl)
-    }
-
-    if (grid[cell.row - 1]) { // check top row
-        if (tr && !tr.dimmed) dimCell(tr)
-        if (t && !t.dimmed) dimCell(t)
-        if (tl && !tl.dimmed) dimCell(tl)
-    }
-}
-
-export function dimCell(cell: CellType) {
-    if (moves.value === 0) {
-        grid[cell.row][cell.col].bomb = true
-        generateBombs(globalWidth, globalHeight, globalBombs)
-        grid[cell.row][cell.col].bomb = false
-        generateNumbers(globalWidth, globalHeight)
-    }
-
-    if (cell.flag) {
-        return
-    }
-
-    if (gameOver.value || win.value) {
-        return
-    }
-
-    if (cell.bomb) {
-        dimAllBombs()
-        gameOver.value = true
-    }
-
-    if (!cell.dimmed) {
-        cellCount.value -= 1
-    }
-
-    if (cell.dimmed) {
-        return checkAdjacentCells(cell)
-    }
-
-    cell.dimmed = true
-    moves.value++
-
-    if (cell.val === 0) {
-        autoClear(cell)
-    }
-
-    if (cellCount.value === 0) {
-        win.value = true
-    }
-}
-
-function dimAllBombs() {
-    grid.forEach(row => {
-        row.forEach(cell => {
-            if (cell.bomb && !cell.flag) cell.dimmed = true;
-        })
-    })
-}
-
-function autoClear(cell: CellType) {
-    const {r, l, br, b, bl, tr, t, tl} = getAdjacentTiles(cell)
-
-    if (r && !r.dimmed) dimCell(r)
-    if (l && !l.dimmed) dimCell(l)
-
-    if (br && !br.dimmed) dimCell(br)
-    if (b && !b.dimmed) dimCell(b)
-    if (bl && !bl.dimmed) dimCell(bl)
-
-    if (tr && !tr.dimmed) dimCell(tr)
-    if (t && !t.dimmed) dimCell(t)
-    if (tl && !tl.dimmed) dimCell(tl)
-}
-
-export function markFlag(cell: CellType) {
-    if (gameOver.value || win.value || cell.dimmed) return
-    if (!cell.flag) {
-        flags.value -= 1
-    } else {
-        flags.value += 1
-    }
-
-    cell.flag = !cell.flag
-}
-
-function generateGrid(width: number, height: number) {
-    for (let i = 0; i < height; i++) {
-        const row: CellType[] = [];
-
-        for (let j = 0; j < width; j++) {
-            row.push({
-                bomb: false,
-                val: 0,
-                flag: false,
-                dimmed: false,
-                fakeDimmed: false,
-                row: i,
-                col: j,
-            });
-        }
-        grid.push(row);
-    }
-}
-
-function generateBombs(width: number, height: number, bombs: number) {
-    if (!grid) return
-
-    for (let i = 0; i < bombs; i++) {
-        let c: number
-        let r: number
-        do {
-            r = getRandomNumber(0, height - 1)
-            c = getRandomNumber(0, width - 1)
-        }
-        while (grid[r][c].bomb)
-        {
-            grid[r][c].bomb = true
-        }
-    }
-}
-
-function generateNumbers(width: number, height: number) {
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            if (grid[i][j].bomb) {
-                const {r, l, br, b, bl, tr, t, tl} = getAdjacentTiles(grid[i][j])
-
-                if (r) r.val++
-                if (l) l.val++
-
-                if (grid[i + 1]) { // check bottom row
-                    if (br) br.val++
-                    if (b) b.val++
-                    if (bl) bl.val++
-                }
-
-                if (grid[i - 1]) { // check top row
-                    if (tr) tr.val++
-                    if (t) t.val++
-                    if (tl) tl.val++
-                }
-            }
-        }
-    }
-}
-
-function clearOld() {
-    if (grid.length ! > 0) {
-        grid.length = 0
-        gameOver.value = false
-        win.value = false
-    }
-}
-
-export function initGame(width: number, height: number, bombs: number) {
-    clearOld()
-    generateGrid(width, height)
-
-    globalWidth = width
-    globalHeight = height
-    globalBombs = bombs
-
-    flags.value = bombs
-    cellCount.value = (width * height) - bombs
-    moves.value = 0
-}
-
-
 export class Game {
-    public width: number
-    public height: number
-    public bombs: number
-
+    public width: number = 0
+    public height: number = 0
+    public bombs: number = 0
+    public flags: number = 0
+    public cellCount: number = 0
     public grid: CellType[][] = reactive([])
+
+    public gameOver: boolean = false
+    public win: boolean = false
+
+    public moves: number = 0
 
     constructor(width: number, height: number, bombs: number) {
         this.width = width
         this.height = height
         this.bombs = bombs
+        this.flags = bombs
+        this.cellCount = (width * height) - bombs
+
+        this.initGame()
+    }
+
+    initGame() {
+        this.clearOld()
+
+        this.generateGrid()
+    }
+
+    clearOld() {
+        if (this.grid.length ! > 0) {
+            this.grid.length = 0
+            this.moves = 0
+            this.gameOver = false
+            this.win = false
+        }
+    }
+
+    generateGrid() {
+        for (let i = 0; i < this.height; i++) {
+            const row: CellType[] = [];
+
+            for (let j = 0; j < this.width; j++) {
+                row.push({
+                    bomb: false,
+                    val: 0,
+                    flag: false,
+                    dimmed: false,
+                    fakeDimmed: false,
+                    row: i,
+                    col: j,
+                });
+            }
+            this.grid.push(row);
+        }
+    }
+
+    clickCell(cell: CellType) {
+        if (this.moves === 0) {
+            this.grid[cell.row][cell.col].bomb = true
+            this.generateBombs()
+            this.grid[cell.row][cell.col].bomb = false
+            this.generateNumbers()
+        }
+
+        if (cell.flag) {
+            return
+        }
+
+        if (this.gameOver || this.win) {
+            return
+        }
+
+        if (cell.bomb) {
+            this.dimAllBombs()
+            this.gameOver = true
+        }
+
+        if (!cell.dimmed) {
+            this.cellCount -= 1
+        }
+
+        if (cell.dimmed) {
+            return this.checkAdjacentCells(cell)
+        }
+
+        cell.dimmed = true
+        this.moves++
+
+        if (cell.val === 0) {
+            this.autoClear(cell)
+        }
+
+        if (this.cellCount === 0) {
+            this.win = true
+        }
+
+        console.log(this.moves)
+    }
+
+    markFlag(cell: CellType) {
+        if (this.gameOver || this.win || cell.dimmed) return
+        if (!cell.flag) {
+            this.flags -= 1
+        } else {
+            this.flags += 1
+        }
+
+        cell.flag = !cell.flag
+    }
+
+    generateBombs() {
+        if (!this.grid) return
+
+        for (let i = 0; i < this.bombs; i++) {
+            let c: number
+            let r: number
+            do {
+                r = getRandomNumber(0, this.height - 1)
+                c = getRandomNumber(0, this.width - 1)
+            }
+            while (this.grid[r][c].bomb)
+            {
+                this.grid[r][c].bomb = true
+            }
+        }
+    }
+
+    generateNumbers() {
+        for (let i = 0; i < this.height; i++) {
+            for (let j = 0; j < this.width; j++) {
+                if (this.grid[i][j].bomb) {
+                    const {r, l, br, b, bl, tr, t, tl} = this.getAdjacentTiles(this.grid[i][j])
+
+                    if (r) r.val++
+                    if (l) l.val++
+
+                    if (this.grid[i + 1]) { // check bottom row
+                        if (br) br.val++
+                        if (b) b.val++
+                        if (bl) bl.val++
+                    }
+
+                    if (this.grid[i - 1]) { // check top row
+                        if (tr) tr.val++
+                        if (t) t.val++
+                        if (tl) tl.val++
+                    }
+                }
+            }
+        }
+    }
+
+    getAdjacentTiles(cell: CellType) {
+        const r = this.grid[cell.row][cell.col + 1]
+        const l = this.grid[cell.row][cell.col - 1]
+
+        let br, b, bl;
+
+        if (this.grid[cell.row + 1]) {
+            br = this.grid[cell.row + 1][cell.col + 1]
+            b = this.grid[cell.row + 1][cell.col]
+            bl = this.grid[cell.row + 1][cell.col - 1]
+        }
+
+        let tr, t, tl;
+
+        if (this.grid[cell.row - 1]) {
+            tr = this.grid[cell.row - 1][cell.col + 1]
+            t = this.grid[cell.row - 1][cell.col]
+            tl = this.grid[cell.row - 1][cell.col - 1]
+        }
+
+        return {r, l, br, b, bl, tr, t, tl}
+    }
+
+    checkAdjacentCells(cell: CellType) {
+        const flagsNearCell = this.getAmountOfFlagsAdjacentToCell(cell)
+        this.shortDim(cell)
+
+        if (flagsNearCell === cell.val) {
+            this.dimAdjacentTiles(cell)
+        }
+    }
+
+    getAmountOfFlagsAdjacentToCell(cell: CellType) {
+        let flagsNearCell = 0;
+
+        const {r, l, br, b, bl, tr, t, tl} = this.getAdjacentTiles(cell)
+
+        if (r && r.flag) flagsNearCell++
+        if (l && l.flag) flagsNearCell++
+
+        if (this.grid[cell.row + 1]) { // check bottom row
+            if (br && br.flag) flagsNearCell++
+            if (b && b.flag) flagsNearCell++
+            if (bl && bl.flag) flagsNearCell++
+        }
+
+        if (this.grid[cell.row - 1]) { // check top row
+            if (tr && tr.flag) flagsNearCell++
+            if (t && t.flag) flagsNearCell++
+            if (tl && tl.flag) flagsNearCell++
+        }
+
+        return flagsNearCell
+    }
+
+    dimAllBombs() {
+        this.grid.forEach(row => {
+            row.forEach(cell => {
+                if (cell.bomb && !cell.flag) cell.dimmed = true;
+            })
+        })
+    }
+
+    autoClear(cell: CellType) {
+        const {r, l, br, b, bl, tr, t, tl} = this.getAdjacentTiles(cell)
+
+        if (r && !r.dimmed) this.clickCell(r)
+        if (l && !l.dimmed) this.clickCell(l)
+
+        if (br && !br.dimmed) this.clickCell(br)
+        if (b && !b.dimmed) this.clickCell(b)
+        if (bl && !bl.dimmed) this.clickCell(bl)
+
+        if (tr && !tr.dimmed) this.clickCell(tr)
+        if (t && !t.dimmed) this.clickCell(t)
+        if (tl && !tl.dimmed) this.clickCell(tl)
+    }
+
+    shortDim(cell: CellType) {
+        this.fakeDim(cell, true)
+        setTimeout(() => {
+            this.fakeDim(cell, false)
+        }, 75)
+    }
+
+    dimAdjacentTiles(cell: CellType) {
+        const {r, l, br, b, bl, tr, t, tl} = this.getAdjacentTiles(cell)
+
+        if (r && !r.dimmed) this.clickCell(r)
+        if (l && !l.dimmed) this.clickCell(l)
+
+        if (this.grid[cell.row + 1]) { // check bottom row
+            if (br && !br.dimmed) this.clickCell(br)
+            if (b && !b.dimmed) this.clickCell(b)
+            if (bl && !bl.dimmed) this.clickCell(bl)
+        }
+
+        if (this.grid[cell.row - 1]) { // check top row
+            if (tr && !tr.dimmed) this.clickCell(tr)
+            if (t && !t.dimmed) this.clickCell(t)
+            if (tl && !tl.dimmed) this.clickCell(tl)
+        }
+    }
+
+    fakeDim(cell: CellType, dimmed: boolean) {
+        const {r, l, br, b, bl, tr, t, tl} = this.getAdjacentTiles(cell)
+
+        if (r && !r.flag) r.fakeDimmed = dimmed
+        if (l && !l.flag) l.fakeDimmed = dimmed
+
+        if (this.grid[cell.row + 1]) { // check bottom row
+            if (br && !br.flag) br.fakeDimmed = dimmed
+            if (b && !b.flag) b.fakeDimmed = dimmed
+            if (bl && !bl.flag) bl.fakeDimmed = dimmed
+        }
+
+        if (this.grid[cell.row - 1]) { // check top row
+            if (tr && !tr.flag) tr.fakeDimmed = dimmed
+            if (t && !t.flag) t.fakeDimmed = dimmed
+            if (tl && !tl.flag) tl.fakeDimmed = dimmed
+        }
     }
 }
